e6b65d633e67b4347a31c679fac31f9f586cb6ff
Shawn Kastner <Shawnkastner@hotmail.de>
2022-12-17 12:54:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = `https://pokeapi.co/api/v2/pokemon/charmander`;
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+    console.log('Loaded Pokemon', currentPokemon);
+
+    renderPokemonInfo(currentPokemon);
+}
+
+function renderPokemonInfo(currentPokemon) {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImg').innerHTML = `<img src="${currentPokemon['sprites']['front_default']}>`;
+}
