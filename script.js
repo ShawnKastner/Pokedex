741ba16c4db1fa7312a8d75628027162e1275466
Shawn Kastner <Shawnkastner@hotmail.de>
2022-12-20 10:43:22 +0100

    added animated background

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let pokemonName;
 let pokemonImg;
 let pokemonId;
 let pokemonType;
-let pokemonColor;
 let allPokemon = [];
 let start = 1;
 let limit = 21;
@@ -26,7 +25,7 @@ function renderPokemon() {
     for (let i = 0; i < allPokemon.length; i++) {
         const pokemon = allPokemon[i];
 
-        pokemonName = pokemon['name'];
+        pokemonName = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
         pokemonImg = pokemon['sprites']['other']['home']['front_default'];
         pokemonId = pokemon['id'];
         pokemonType = pokemon['types'][0]['type']['name'];
@@ -34,7 +33,8 @@ function renderPokemon() {
         content.innerHTML += /*html*/`
         <div class="card" style="background-color:${pokemonColor};">
             <div>
-                <h2>#${pokemonId};${pokemonName}</h2>
+                <h3  class="nameIdAbsolute">#${pokemonId}</h3>
+                <h2>${pokemonName}</h2>
             </div>
             <div>
                 <span>${pokemonType}</span>
@@ -102,58 +102,3 @@ function renderPokemonInfo(currentPokemon) {
 //     `;
 // }
 
-function colorOfPokemon(pokemonType) {
-    if(pokemonType == 'fire') {
-        return pokemonColor = 'rgb(254, 118, 118)';
-    }
-    if(pokemonType == 'grass') {
-        return pokemonColor = 'rgb(76, 206, 177)';
-    }
-    if(pokemonType == 'water') {
-        return pokemonColor = 'rgb(118, 190, 254)';
-    }
-    if(pokemonType == 'bug') {
-        return pokemonColor = 'rgb(108, 177, 80)';
-    }
-    if(pokemonType == 'normal') {
-        return pokemonColor = 'rgb(228, 220, 220)';
-    }
-    if(pokemonType == 'poison') {
-        return pokemonColor = 'rgb(147, 103, 169)';
-    }
-    if(pokemonType == 'electric') {
-        return pokemonColor = 'rgb(240, 216, 52)';
-    }
-    if(pokemonType == 'fairy') {
-        return pokemonColor = 'rgb(207, 65, 121)';
-    }
-    if(pokemonType == 'fighting') {
-        return pokemonColor = 'rgb(195, 128, 96)';
-    }
-    if(pokemonType == 'ground') {
-        return pokemonColor = 'rgb(133, 78, 56)';
-    }
-    if(pokemonType == 'psychic') {
-        return pokemonColor = 'rgb(171, 123, 174)';
-    }
-    if(pokemonType == 'rock') {
-        return pokemonColor = 'rgb(207, 195, 147)';
-    }
-    if(pokemonType == 'ghost') {
-        return pokemonColor = 'rgb(153, 123, 154)';
-    }
-    if(pokemonType == 'ice') {
-        return pokemonColor = 'rgb(157, 221, 210)';
-    }
-    if(pokemonType == 'dragon') {
-        return pokemonColor = 'rgb(136, 154, 209)';
-    }
-    if(pokemonType == 'dark') {
-        return pokemonColor = 'rgb(131, 124, 124)';
-    }
-    if(pokemonType == 'steel') {
-        return pokemonColor = 'rgb(195, 195, 207)';
-    }
-
-    return pokemonColor;
-}
